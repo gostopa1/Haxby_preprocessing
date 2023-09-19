ac41bd4cc63526bbc239ab3e7715499aad2c2cbc
Athanasios Gotsopoulos <athanasios.gotsopoulos@aalto.fi>
2023-09-19 20:59:23 +0300

    Cleaned up script from empty space

--- unzip_the_gz_BOLD_niis.m
+++ unzip_the_gz_BOLD_niis.m
@@ -1,11 +1,8 @@
 %% This script unzips the nii files that are compressed in gz format
 
-
 prefix1='./ds105/'
 
-
 res1=dir([prefix1 'sub*']);
-
 
 for res1i=1:length(res1)
     prefix2=[prefix1 res1(res1i).name '/BOLD/']
@@ -13,15 +10,9 @@ for res1i=1:length(res1)
     for res2i=1:length(res2)
        res3=dir([prefix2 res2(res2i).name '/*gz']);
        filename=[prefix2 res2(res2i).name '/' res3.name];
-       
-       %pause
-       string=['gzip -d ' filename]
-       system(['gzip -d ' filename])
-       
-        
-    end
-    
-    
-    
+       cmd=['gzip -d ' filename]
+
+       system(cmd);
+    end  
 end
 
